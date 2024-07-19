6c1e221af8b125e7e08f27e74ab85decebe70fb8
HiramMasso <hiram.5658@gmail.com>
2024-07-19 11:28:03 -0400

    feat: Hiram_Masso_Python_Hack1

--- hack_10.py
+++ hack_10.py
@@ -3,6 +3,17 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 """
 
 def fn_hack_10():
-    result = "fooziman"
-    #...
-    return result  
+    mapping = {
+        'f': 'F',
+        'o': '0',
+        'z': 'Z',
+        'i': '1',
+        'm': 'M',
+        'a': '@',
+        'n': 'N'
+    }
+    result = [""] * len("fooziman")
+    for index, char in enumerate("fooziman"):
+        result[index] = mapping[char]
+    
+    return result
--- hack_4.py
+++ hack_4.py
@@ -4,5 +4,5 @@ text: "fooziman" output => "foozimaN"
 
 def fn_hack_4():
     result = "fooziman"
-    #...
+    result = result[:-1] + "N"
     return result
